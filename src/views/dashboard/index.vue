241204a1a100b3f2932d5040d7b166ef21ac72fb
lrf <lrf@meitu.com>
2022-03-03 18:25:47 +0800

    first commit

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Dashboard page</h1>
+    <SvgIcon name="404" />
+    <el-button>按钮</el-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'Dashboard',
+  setup() {
+
+  },
+})
+</script>
